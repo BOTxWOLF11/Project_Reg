cad6f845f944a7647f6fbd29eb1a2861e7e03579
BOTxWOLF11 <95867390+BOTxWOLF11@users.noreply.github.com>
2023-09-10 22:30:53 +0300

    Add files via upload

---
+++ WindowsFormsApp10/WindowsFormsApp10/Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string companyName = textBox1.Text;
+            string companyAddress = textBox4.Text;
+            string Date = dateTimePicker1.Text;
+            string companySUMM = textBox2.Text;
+
+            // Проверка, что все поля заполнены
+            if (string.IsNullOrEmpty(companyName) || string.IsNullOrEmpty(companyAddress) || string.IsNullOrEmpty(companySUMM))
+            {
+                MessageBox.Show("Пожалуйста, заполните все поля.");
+                return;
+            }
+
+            // Создание строки с данными компании
+            string companyData = $"Название компании: {companyName}\r\nАдрес компании: {companyAddress}\r\nДата Создания: {Date}\r\nСумма компании: {companySUMM}\r\n";
+
+            try
+            {
+                // Запись данных в текстовый файл
+                string filePath = "company_data.txt";
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+                {
+                    writer.WriteLine(companyData);
+                }
+
+                MessageBox.Show("Компания успешно зарегистрирована.");
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при регистрации компании: {ex.Message}");
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+   }
+}
